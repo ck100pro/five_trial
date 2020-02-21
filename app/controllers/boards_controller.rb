92dcef6a31fed3d3fdab30be1afbac8db9af4d93
ck100pro <ck100protf10@gmail.com>
2020-02-21 22:42:10 +0800

    create_board_view_flash

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,3 +1,19 @@
 class BoardsController < ApplicationController
+  before_action :board_params, only: [:create]
 
+  def index
+    @board = Board.new
+  end
+
+  def create
+    board = Board.new(board_params)
+    if board.save
+      redirect_to root_path, {notice: "board建立成功"}
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title)
+  end
 end
