5084f03a0c6fcb8bd8ceb547e752a73e631419f9
ck100pro <ck100protf10@gmail.com>
2020-03-03 23:33:31 +0800

    add_card_create

--- app/javascript/script/show_controller/templete/listview.vue
+++ app/javascript/script/show_controller/templete/listview.vue
@@ -2,7 +2,14 @@
   <div class="flex">
     <div v-for="value in title">
       <div class="h-8 w-64 border-black border-solid border-2 rounded">
-        <span :id=value.id class="block h-full w-full cursor-pointer">{{value.title}}</span>
+        <span :id="value.id" class="block h-full w-full cursor-pointer">{{value.title}}</span>
+      </div>
+      <div class="h-8 w-64 border-black border-solid border-2 rounded">
+        <span class="block h-full w-full cursor-pointer">新增卡片</span>
+      </div>
+      <div class="h-8 w-64 border-black border-solid border-2 rounded">
+        <input class="h-full w-full" type="text" v-model="cardTitle">
+        <button @click="cardClick">送出</button>
       </div>
     </div>
   </div>
@@ -10,8 +17,19 @@
 
 <script>
 export default {
+  data: () => {
+    return {
+      cardTitle: undefined,
+      listId: undefined
+    }
+  },
   props: [
     "title"
   ],
+  methods: {
+    cardClick: function(){
+
+    }
+  }
 }
 </script>
