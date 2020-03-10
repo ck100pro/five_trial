7fd54d2efec5adf76a986ecfe7bc785e8a9b81f5
ck100pro <ck100protf10@gmail.com>
2020-03-10 11:54:05 +0800

    add_vue_vuex

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,9 @@ require("channels")
 import '@fortawesome/fontawesome-free/css/all.css'
 import Vue from 'vue/dist/vue.esm'
 import axios from "../helpers/axios.js"
+import Vuex from 'vuex'
 
 global.Vue = Vue;
 global.axios = axios;
+
+Vue.use(Vuex)
