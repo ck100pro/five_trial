bcefa10034da8ba86a96e63a7e0d64f330313fe8
ck100pro <ck100protf10@gmail.com>
2020-03-13 23:25:50 +0800

    fix_cards_rspec

--- spec/controller/cards_controller_spec.rb
+++ spec/controller/cards_controller_spec.rb
@@ -1,48 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe CardsController, type: :controller do
-  before(:all) do
-    @board = FactoryBot.create(:board)
-    @list = FactoryBot.create(:list, board_id: @board.id)
-    @card = FactoryBot.create(:card, list_id: @list.id)
-  end
+  let!(:board) { create(:board) }
+  let!(:list) { create(:list, board_id: board.id) }
 
   describe "#create" do
-    before(:all) do
-      @params = {card: {title: "title"}, board_id: @board.id, list_id: @list.id, format: :json}
-    end
+    let!(:params) { {card: {title: "title"}, board_id: board.id, list_id: list.id, format: :json} }
 
     it "create success " do
-      post :create, params: @params
+      expect{ post :create, params: params }.to change{ Card.count}.by(1)
+      expect(Card.last.title).to eq("title")
       expect(response).to have_http_status(200)
       expect(response.content_type).to eq "application/json; charset=utf-8"
     end
 
     it "create fail" do
       allow_any_instance_of(Card).to receive(:save).and_return(false)
-      post :create, params: @params
+      expect{ post :create, params: params }.to change{ Card.count }.by(0)
       expect(response).to have_http_status(400)
       expect(response.content_type).to eq "application/json; charset=utf-8"
     end
   end
 
   describe "delete success" do
-    before(:all) do
-      @params = {board_id: @board.id, list_id: @list.id, id: @card.id, format: :json}
-    end
+    let!(:card) { create(:card, :list_id => list.id) }
+    let!(:params) { {board_id: board.id, list_id: list.id, id: card.id, format: :json} }
 
     it "destroy success" do 
-      expect{delete :destroy, params: @params}.to change{Card.all.size}.by(-1)
-      expect(Card.find_by(id: @card.id)).to be nil
+      expect{delete :destroy, params: params}.to change{ Card.count }.by(-1)
+      expect(Card.find_by(id: card.id)).to be_nil
       expect(response).to have_http_status(200)
       expect(response.content_type).to eq "application/json; charset=utf-8"
-      # expect(response.body).to eq "刪除成功"
-      # p "----------#{response.body}-----------------"
     end
 
     it "destroy fail" do 
       allow_any_instance_of(Card).to receive(:destroy).and_return(false)
-      expect{delete :destroy, params: @params}.to change{Card.all.size}.by(0)
+      expect{ delete :destroy, params: params }.to change{ Card.count }.by(0)
+      expect(Card.find_by(id: card.id)).to eq (card)
       expect(response).to have_http_status(400)
       expect(response.content_type).to eq "application/json; charset=utf-8"
     end
