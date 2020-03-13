821314e0cbf76f5c3350e916d0111553fe892e79
ck100pro <ck100protf10@gmail.com>
2020-03-13 23:54:56 +0800

    fix_lists_rspec

--- spec/controller/lists_controller_spec.rb
+++ spec/controller/lists_controller_spec.rb
@@ -1,46 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe ListsController, type: :controller do
-  before(:all) do
-    @board = FactoryBot.create(:board)
-    @list = FactoryBot.create(:list, board_id: @board.id)
-  end
+  let!(:board) { create(:board) }
 
   describe "#create" do
-    before(:all) do
-      @params = {list: {title: "title"}, board_id: @board.id, format: :json }
-    end
+    let!(:params) { { list: {title: "title"}, board_id: board.id, format: :json } }
 
     it "create success" do
-      expect{post :create, params: @params}.to change{List.all.size}.by(1)
+      expect{ post :create, params: params }.to change{ List.count }.by(1)
+      expect( List.last.title).to eq("title")
       expect(response).to have_http_status(200)
       expect(response.content_type).to eq "application/json; charset=utf-8"
     end
 
     it "create error" do
       allow_any_instance_of(List).to receive(:save).and_return(false)
-      post :create, params: @params
+      expect{ post :create, params: params }.to change{ List.count }.by(0)
       expect(response).to have_http_status(400)
       expect(response.content_type).to eq "application/json; charset=utf-8"
     end
   end
 
   describe "#update" do
-    before(:all) do
-      @params = {list: {title: "update"}, board_id: @board.id, id: @list.id, format: :json}
-    end
+    let!(:list) { create(:list, :board_id => board.id)}
+    let!(:params) { {list: {title: "update"}, board_id: board.id, id: list.id, format: :json} }
 
     it "update success" do
-      patch :update, params: @params
-      # expect(list.reload.title).to eq('update')
-      expect(List.find(@list.id).title).to eq "update"
+      expect{ patch :update, params: params }.to change{ List.count }.by(0)
+      expect(list.reload.title).to eq('update')
       expect(response.content_type).to eq "application/json; charset=utf-8"
     end
 
     it "update error" do
       allow_any_instance_of(List).to receive(:update).and_return(false)
-      patch :update, params: @params
-      expect(List.find(@list.id).title).to eq "#{@list.title}"
+      expect{ patch :update, params: params }.to change{ List.count }.by(0)
+
+      expect(list.reload.title).not_to eq ("update")
       expect(response.content_type).to eq "application/json; charset=utf-8"
     end
   end
