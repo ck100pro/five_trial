577f30bc17cda20fc7df308c38cbb1616b7cd612
ck100pro <ck100protf10@gmail.com>
2020-03-22 20:54:23 +0800

    test_seting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -109,14 +109,20 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  Capybara.register_driver :chrome do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome, args: ['--window-size=1280,1024'])
   end
   
   Capybara.register_driver :headless_chrome do |app|
-    options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu disable-dev-shm-usage])
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w(headless disable-gpu no-sandbox --window-size=1280,1024) }
+    )
   
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: capabilities
+    )
   end
   
   Capybara.javascript_driver = :headless_chrome
